1f6925d0f38ccf87979830800c25cd183bc8985a
Yangyang Lin <112920815+326-YL@users.noreply.github.com>
2022-10-23 20:52:50 -0400

    Delete doc directory

--- doc/script.js
+++
@@ -1,9 +0,0 @@
-function gameDropdown(console) {
-    const consoleEl = document.getElementById(console);
-    const gameEl = document.getElementById(console+"-game");
-    if (gameEl.classList.contains("game-hidden")) {
-        gameEl.classList.remove("game-hidden");
-    } else {
-        gameEl.classList.add("game-hidden");
-    }
-}
