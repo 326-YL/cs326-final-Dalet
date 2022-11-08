8fc6846d4da2e469a30f11689f50577108187275
Yangyang Lin <112920815+326-YL@users.noreply.github.com>
2022-11-08 12:46:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,23 +2,32 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const http=require('http')
+const httpServer=http.createServer(app);
+httpServer.listen(process.env.PORT|| 443)
+
+//to include all assets
+app.use(express.static(path.join(__dirname,'public')));
+
+
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/public/index.html'));
-  //__dirname : It will resolve to your project folder.
+  //__dirname : It will resolve to the project folder.
 });
 
-/*router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/about.html'));
+
+
+router.get('/Explore',function(req,res){
+
+  res.sendFile(path.join(__dirname+'/public/explore.html'));
 });
 
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
-});*/
+router.get('/My Collection',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/collection.html'));
+});
 
 //add the router
 app.use('/', router);
 
-app.listen(process.env.PORT|| 443);
-
-console.log('Running at Port 3000');
+//app.listen(process.env.PORT|| 443);
