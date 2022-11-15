5ced58bce0c15d3b73481859322ab6c3bcd024cf
BuildTools <unconfigured@null.spigotmc.org>
2022-11-14 19:47:50 -0500

    Fixed explore link

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ router.get('/index',function(req,res){
 
 
 router.get('/explore',function(req,res){
-  res.sendFile(path.join(__dirname+'/explore.html'));
+  res.sendFile(path.join(__dirname+'/public/explore.html'));
 });
 
 router.get('/collection',function(req,res){
