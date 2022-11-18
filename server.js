208464933135d5ffdb2023f0b1ed7c4f51379f0f
BuildTools <unconfigured@null.spigotmc.org>
2022-11-18 18:53:57 -0500

    Testing data

--- server.js
+++ server.js
@@ -118,13 +118,14 @@ router.get('/db', async (req, res) => {
       );`);
     // const result = await client.query("INSERT INTO test (uid, username,password) VALUES (1, 'test', 'test');");
     const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
+    res.send(results);
+    // res.render('pages/db', results );
     client.release();
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
   }
-})
+});
 
 //Stuff idk idc about
 
