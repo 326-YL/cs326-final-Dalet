4238da8ef5b9c45583dfaf8ce307771052dc3a5b
BuildTools <unconfigured@null.spigotmc.org>
2022-11-19 08:48:55 -0500

    adding n64

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ router.get('/db', async (req, res) => {
   try {
     const client = await pool.connect();
     // await client.query("DROP TABLE consoles");
-    let data = JSON.parse(fs.readFileSync('./console_data/snes-data.json'));
+    let data = JSON.parse(fs.readFileSync('./console_data/n64-data.json'));
     await client.query(`CREATE TABLE IF NOT EXISTS consoles (
       cid SERIAL,
       brand varchar(255),
