f0f47a324298d31b5dfca19ff754e0f14d3af63a
yangyang lin <yangyanglin@umass.edu>
2022-12-08 21:19:54 -0500

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,23 @@
 /**
  * fetch game news data from api
  */
-async function getNews(){
-  
+ const axios = require("axios");
 
-
-
-
-
-}
+ const options = {
+   method: 'GET',
+   url: 'https://videogames-news2.p.rapidapi.com/videogames_news/search_news',
+   params: {query: 'GTA'},
+   headers: {
+     'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+     'X-RapidAPI-Host': 'videogames-news2.p.rapidapi.com'
+   }
+ };
+ 
+ axios.request(options).then(function (response) {
+     console.log(response.data);
+ }).catch(function (error) {
+     console.error(error);
+ });
 /**
  * fetch
  */
